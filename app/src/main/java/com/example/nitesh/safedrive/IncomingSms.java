f992848c30d7558277b0f3022ae583a0a53370b8
nitrek <koolestnitesh@gmail.com>
2016-08-25 00:14:53 +0530

    adding readout

--- app/src/main/java/com/example/nitesh/safedrive/IncomingSms.java
+++ app/src/main/java/com/example/nitesh/safedrive/IncomingSms.java
@@ -4,10 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 /**
  * Created by nitesh on 23-08-2016.
@@ -34,7 +37,21 @@ public class IncomingSms extends BroadcastReceiver {
 
                     String senderNum = phoneNumber;
                     String message = currentMessage.getDisplayMessageBody();
+                    final TextToSpeech t1 = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+                       @Override
+                        public void onInit(int status) {
+                            if(status != TextToSpeech.ERROR) {
+                                try {
+                                    t1.setLanguage(Locale.UK);
+                                }
+                                catch (Exception e)
+                                {
 
+                                }
+                            }
+                        }
+                    });
+                    t1.speak(message, TextToSpeech.QUEUE_FLUSH, null);
                     Log.i("SmsReceiver", "senderNum: "+ senderNum + "; message: " + message);
                     int duration = Toast.LENGTH_LONG;
                     Toast toast = Toast.makeText(context,
